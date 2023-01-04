be89fa7ee7bdf06cb46b4f602a3cf246003849f9
barshasethi456@gmail.com <barshasethi456@gmail.com>
2023-01-04 22:51:57 +0530

    first commit

--- One.java
+++ One.java
@@ -1,7 +1,8 @@
 public class One{
 
      public static void main(String[] args){
-         System.out.println("hello world");
+        String myName = "hi my name is barsha";
+         System.out.println(myName);
    
      }
 }
